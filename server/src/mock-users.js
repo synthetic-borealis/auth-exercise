43ba68ff2df9589112a2a93f23fe834db9c9d5bd
Frazonar <snip.coloured@gmail.com>
2022-08-07 11:57:45 +0300

    Add more mock users

---
+++ server/src/mock-users.js
@@ -0,0 +1,5 @@
+module.exports = [
+  { email: 'duck@duckmail.org', password: 'Password' },
+  { email: 'user@email.com', password: '1234Pass' },
+  { email: 'gordon.freeman@blackmesa.com', password: 'Guess1234'}
+];
--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,8 @@ const helmet = require('helmet').default;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const mockUsers = require('./mock-users');
+
 const server = express();
 
 server.options('*', cors());
@@ -11,15 +13,10 @@ server.use(cors());
 server.use(bodyParser.json());
 server.use(helmet());
 
-const mockUser = {
-  email: 'duck@duckmail.org',
-  password: 'Password',
-};
-
 server.post('/', (req, res) => {
   const { email, password } = req.body;
 
-  if (email === mockUser.email && password === mockUser.password) {
+  if (mockUsers.find((user) => user.email === email && user.password === password)) {
     res.status(200).json({ message: 'OKAY?' });
   } else {
     res.status(401).json({ message: 'You shall not pass!' });
